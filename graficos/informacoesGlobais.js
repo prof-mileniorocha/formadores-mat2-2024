a6ddfcec70514d7e80a1a4108efe02051c6774d7
prof-mileniorocha <127246074+prof-mileniorocha@users.noreply.github.com>
2024-11-07 23:32:32 +0000

    Atualizando aula

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -3,7 +3,17 @@ const url='https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGr
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
-    console.log(dados)
+    const pessoasConectadas = (dados.total_pessoas_que_usam_meios_de_comunicacao / 1e9);
+    const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9);
+    const horas = parseInt(dados.tempo_medio_dia_usando_meios_de_comunicacao);
+    const minutos = Math.round((dados.tempo_medio_dia_usando_meios_de_comunicacao - horas)*60);
+    const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo) * 100).toFixed(2);
+
+    const paragrafo = document.createElement('p');
+    paragrafo.classList.add('graficos-container__texto');
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span> ${pessoasNoMundo} bilhões</span> de pessoas e que aproximadamente <span> ${pessoasConectadas} bilhões </span> estão conectadas em alguma rede social e passam em média <span> ${horas} </span> horas e <span> ${minutos} minutos conectadas por dia.</span> <br> Isso significa que aproximadamente <span>${porcentagemConectada}%</span> pessoas conectada em alguma rede social.`
+    const container = document.getElementById('graficos-container');
+    container.appendChild(paragrafo);
 }
 
 vizualizarInformacoesGlobais()
